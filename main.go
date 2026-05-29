6f1fbfe2a11b06f5347fa041cea0a5138e6c44cf
kirqe <ikll7@yahoo.com>
2026-05-30 02:15:40 +0400

    Works?

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	e.Use(middleware.RequestLogger())
 
 	e.GET("/", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!!")
 	})
 
 	if err := e.Start(":8080"); err != nil {
